5d339ee43abb325cb00e5c6d7c0152c229dd0ca5
CryptoAutistic80 <jameswalford@outlook.com>
2024-09-14 14:38:40 +0100

    Initial commit

---
+++ .cursor-server/bin/63dae81aea2c3fc079420fe72578a8eef26ba870/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="cursor"
+VERSION="0.40.4"
+COMMIT="63dae81aea2c3fc079420fe72578a8eef26ba870"
+EXEC_NAME="cursor"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
